dbf5da44b113c140a530f74154326e3fa4cbf004
Etse7 <gizawgete27@yahoo.com>
2022-04-03 16:15:52 -0700

    static libraries

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -6,9 +6,9 @@
  */
 int _strlen(char *s)
 {
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+	return (inc);
+}
